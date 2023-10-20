379dbcd9ed28b36f70874a56aafac1461081ca54
christianLizama <ckill661@gmail.com>
2023-10-19 22:24:30 -0300

    Agregación de mui y creación de una navbar

---
+++ src/app/components/ui/Navbar.tsx
@@ -0,0 +1,15 @@
+import { AppBar, IconButton, Toolbar } from '@mui/material'
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <AppBar position='sticky' elevation={0}>
+        <Toolbar>
+            <IconButton>
+                <MenuOutlinedIcon/>
+            </IconButton>
+        </Toolbar>
+    </AppBar>
+  )
+}
