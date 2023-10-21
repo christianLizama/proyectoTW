5b9ba82163f25bd81055d769849fceb41f62e523
serllet <sfuentes18@alumnos.utalca.cl>
2023-10-20 22:47:31 -0300

    Login frontend
    Se creo el componente SignIn.Cliente el cual contiene el form del login

--- src/app/components/layouts/Layout.tsx
+++ src/app/components/layouts/Layout.tsx
@@ -1,25 +1,44 @@
 import Box from '@mui/material/Box';
 import Head from 'next/head';
 import { FC } from 'react';
-import NavBar from "../ui/Navbar";
+import SignIn from '../cliente/SignIn.client';
 
 interface Props {
     title?: string; 
     children: React.ReactNode;
 }
 
-export const Layout:FC<Props> = ({title = 'Prueba',children}) => {
+export const Layout:FC<Props> = ({title = 'Prueba', children}) => {
   return (
-    <Box sx={{ flexFlow : 1}}>
+    <Box 
+      sx={{ 
+        display: 'flex', 
+        flexDirection: 'column', 
+        alignItems: 'center', 
+        justifyContent: 'center',
+        height: '100vh', // Hacer que el contenedor principal ocupe toda la altura de la ventana
+        bgcolor: 'white',
+        color: 'black' // Cambiar el color del texto a negro
+      }}
+    >
         <Head>
             <title>{title}</title>
         </Head>
-            <NavBar/>
-
-            <Box sx={{padding:'10px 20px'}}>
-                {children}
-            </Box>
-
+        {/* Puedes descomentar la siguiente línea para mostrar el NavBar si es necesario
+        <NavBar/> 
+        */}
+        <SignIn/>
+        <Box 
+          sx={{
+            padding: '10px 20px',
+            maxWidth: '500px', // Ajustar según sea necesario para cambiar el ancho máximo del contenedor del contenido
+            width: '100%', // Asegurarse de que el contenedor del contenido sea receptivo
+            bgcolor: 'white',
+            color: 'black' // Asegurarse de que el texto dentro de este contenedor también sea negro
+          }}
+        >
+            {children}
+        </Box>
     </Box>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { Layout } from "./components/layouts";
 const HomePage: NextPage = () => {
   return (
     <Layout>
-      <Typography variant="h1">Hello world</Typography>
+      
     </Layout>
   );
 };
